5fa5a910a561cb169819dde677c0e8777dae0a22
filipemartinsdev <dev.filipemartins@gmail.com>
2026-03-07 14:26:26 -0300

    fix: add web dependency and env for up service

--- src/main/java/br/social/impacthub/config/security/SecurityConfig.java
+++ src/main/java/br/social/impacthub/config/security/SecurityConfig.java
@@ -2,12 +2,12 @@ package br.social.impacthub.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,6 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Lazy
     private AuthenticationFilter authenticationFilter;
 
     public SecurityConfig(AuthenticationFilter authenticationFilter) {
--- src/main/java/br/social/impacthub/repository/UserProfileRepository.java
+++ src/main/java/br/social/impacthub/repository/UserProfileRepository.java
@@ -3,8 +3,11 @@ package br.social.impacthub.repository;
 import br.social.impacthub.model.entity.UserCredentials;
 import br.social.impacthub.model.entity.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, UUID> {
+    List<UserProfile> findByUsername(String username);
 }
